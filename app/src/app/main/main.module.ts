5d18837d7939f44b4f5490657d464eb448bb3f24
Ihar Kozik <ihar.kozik@gmail.com>
2022-08-28 21:15:51 +0300

    feat: add welcome page

--- app/src/app/main/main.module.ts
+++ app/src/app/main/main.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WelcomeComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
