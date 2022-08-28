96255df09a5ada1968cfb743e8ea1502334f4cfe
Ihar Kozik <ihar.kozik@gmail.com>
2022-08-28 21:40:01 +0300

    feat: add SearchingService as DI to WorkOrdersComponent

--- app/src/app/main/components/work-orders/work-orders.component.ts
+++ app/src/app/main/components/work-orders/work-orders.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WorkOrder } from '../../models/work-order.model';
+import { SearchingService } from '../../services/searching.service';
 
 @Component({
   selector: 'app-work-orders',
   templateUrl: './work-orders.component.html',
-  styleUrls: ['./work-orders.component.scss']
+  styleUrls: ['./work-orders.component.scss'],
 })
-export class WorkOrdersComponent implements OnInit {
+export class WorkOrdersComponent implements OnInit, OnDestroy {
+  items: WorkOrder[];
+  private itemsSubs: Subscription | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private searchingService: SearchingService) {
+    this.items = [];
+    this.itemsSubs = null;
   }
 
+  ngOnInit(): void {
+    this.searchingService.getInitialData();
+    this.itemsSubs = this.searchingService.workOrders$.subscribe(
+      (workOrders) => (this.items = workOrders)
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.itemsSubs) this.itemsSubs.unsubscribe();
+  }
 }
